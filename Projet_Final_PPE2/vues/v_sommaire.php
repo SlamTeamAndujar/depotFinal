8aa89731d3acded4f43f9c0d8c9c025264f8e75c
Mariana Andujar <mariana.andujarf@gmail.com>
2013-03-30 16:29:45 +0100

    Raissa update

--- Projet_Final_PPE2/vues/v_sommaire.php
+++ Projet_Final_PPE2/vues/v_sommaire.php
@@ -10,23 +10,30 @@
 				 Bienvenue :<br>
 				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
 			</li>
+                        </br>
               <li class="smenu">
-                   Comptes rendus 
-           </li> 
+                   Comptes rendus :
+           </li>
+           </br>
            <li class="smenu">
-              <a href="index.php?uc=gererCR&action=saisirCR=" title="Nouveaux comptes rendus">Nouveaux</a>
+              <a href="index.php?uc=gererCR&action=saisirCR" title="Nouveaux comptes rendus">Nouveaux</a>
            </li>
            <li class="smenu">
               <a href="index.php?uc=" title="Consulter les comptes rendus">Consulter</a>
-           </li><li class="smenu">
+           </li>
+           <li class="smenu">
+               </br>
            <li class="smenu">
               Consultations
            </li>
            <li class="smenu">
-              <a href="index.php?uc=consulterMedicaments&action=voirMedicaments" title="Consulter les médicaments">    Médicaments</a>
-           </li><li class="smenu">
-              <a href="index.php?uc=lesPraticien" title="Consulter les praticiens">Praticiens</a>
+              <a href="index.php?uc=consulterMedicaments&action=voirMedicaments" title="Consulter les médicaments">Médicaments</a>
            </li>
+           </br>
+           <li class="smenu">
+              <a href="index.php?uc=lesPraticiens" title="Consulter les praticiens">Praticiens</a>
+           </li>
+           </br>
            <li class="smenu">
               <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
            </li>
