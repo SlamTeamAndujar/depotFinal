fc8120f453cd6364e5c516300a4ed8d1e8013a08
Mariana Andujar <mariana.andujarf@gmail.com>
2013-04-01 21:53:09 +0200

    Update

--- Projet_Final_PPE2/controleurs/c_gererCR.php
+++ Projet_Final_PPE2/controleurs/c_gererCR.php
@@ -13,13 +13,45 @@ switch($action){
         case 'saisirCR':
                 include("vues/v_saisieCR.php");
         break;
-//	case 'validerCR':
-//            if(aze){
-//                
-//            }else{
-//                
-//            }
-//            break;
+	case 'validerCR':
+            $msg="";
+            $matricule= $idVisiteur;
+            $num= $pdo->num();
+            $numPraticien=$_POST['selectPraticien'];        
+            $dateVisite= $_POST['dateVisite'];
+            $bilan= $_POST['bilan'];
+            $medoc1=$_POST['selectProduit1'];
+            $medoc2=$_POST['selectProduit2'];
+            
+            
+            if($_POST['selectMotif'] == 'autreMotif'){
+                $motif=$_POST['autreMotif'];
+            }else{
+                $motif=$_POST['selectMotif'];
+            }
+            try{
+                if( $medoc1 == $medoc2){
+                    $quantite= $_POST['echantillon1']+$_POST['echantillon2'];       
+                    $resultOffrir= $pdo->ajoutOffrir($matricule,$num,$medoc1,$quantite);
+                    $result = $pdo->ajoutRapportVisite($matricule,$num,$numPraticien,$dateVisite,$bilan,$motif);
+                }else{
+                    $quantite1=$_POST['echantillon1'];
+                    $quantite2=$_POST['echantillon2'];
+                        $resultOffrir= $pdo->ajoutOffrir($matricule,$num,$medoc1,$quantite1);
+                        $resultOffrir= $pdo->ajoutOffrir($matricule,$num,$medoc2,$quantite2);
+                        $result = $pdo->ajoutRapportVisite($matricule,$num,$numPraticien,$dateVisite,$bilan,$motif);
+                    }
+            
+            $msg = $msg."Fiche bilan enregistrée !";
+            }
+            catch(Exception $e)
+            {
+                $msg = $msg.$e;
+
+            }
+            include("vues/v_validerCR.php");
+            break;
+            
         case 'annulerCR':
                 include("vues/v_vide.php");
             
